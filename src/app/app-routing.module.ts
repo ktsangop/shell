1dce1d2d2d457ca4ec7028a514dbfacb41f71af7
ktsangop <ktsangop@outlook.com>
2022-09-06 12:28:29 +0300

    - Add dynamic module import - Add some styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'mfe1',
+    loadChildren: () =>
+        loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4201/remoteEntry.js',
+            exposedModule: './Module'
+        })
+        .then(m => m.MicroFrontend1Module)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
